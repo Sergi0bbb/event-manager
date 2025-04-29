50fbbf89e5cc8291d50761d512e39a955b466d38
serhii <ockar2018@gmail.com>
2025-04-29 22:33:40 +0300

    add model for user and migrations

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext
 
-# Create your models here.
+from user.usermanager import UserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(gettext("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
